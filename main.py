e4d8dc74aa0f97649a0bf6cee666c660d3d1e688
Renan Lopes <renanlopessjc@gmail.com>
2022-11-04 06:52:10 -0300

    Second Commit

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-from tkinter import *
-from tkinter import ttk
+from kivy.app import App
+from kivy.lang import Builder
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-root.title('To-Do List')
-root.geometry('300x400')
-root.resizable(0, 0)
-root.config(bg="Black")
+GUI = Builder.load_file("tela.kv")
 
 
-new_item_entry = Entry(root, width=37)
-new_item_entry.place(x=35, y=310)
+class MeuAplicativo(App):
+    def build(self):
+        return GUI
 
-add_btn = Button(root, text='Add Item', bg='Azure', width=10, font=('Helvetica', 12),
 
-root.mainloop()
+MeuAplicativo().run()
